ca341efe59a572b37edaae54072215451771eed5
Francisco Goya <frangoya775@gmail.com>
2023-09-11 18:44:35 -0300

    items

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import { useState, useEffect } from 'react'
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import ItemListContainer from './components/ItemListContainer'
 import NavBar from './components/NavBar'
-
+import ItemList from './components/ItemList'
 
 const App = () => {
   return (
     <>
-    <NavBar/>
-    <ItemListContainer greeting={"Bienvenido a la tienda"}/>
-    </>
-  )
-}
+      <BrowserRouter>
+        <NavBar />
 
-export default App
+        <Routes>
+       
+        <Route exact path="/" element={<ItemListContainer/>}/>
+        <Route path="/categoria/:categoria" element={<ItemListContainer/>}/>
+        </Routes>
+      </BrowserRouter>
+
+
+
+
+
+
+    </>
+  );
+};
+
+export default App;
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,13 +1,45 @@
 import React from 'react'
-import ItemCount from './ItemCount'
+import ItemList from './ItemList';
+
 
 const ItemListContainer = ({ greeting}) => {
-  return (
-    <>
-    <h1>{greeting}</h1>
-    <ItemCount />
-    </>
-  )
-}
+  const productos =[
+    {id:1 ,nombre:"Gollum" ,descripcion: "descripcion de producto",precio:1000},
+    {id:2 ,nombre:"Producto B" ,descripcion: "descripcion de producto",precio:1500},
+    {id:3 ,nombre:"Producto C" ,descripcion: "descripcion de producto",precio:2000},
+    {id:4 ,nombre:"Producto C" ,descripcion: "Flautas dulces",precio:2000},
+    {id:5 ,nombre:"Producto C" ,descripcion: "descripcion de producto",precio:2000},
+    {id:6 ,nombre:"Producto C" ,descripcion: "descripcion de producto",precio:2000},
+    {id:7 ,nombre:"Producto C" ,descripcion: "descripcion de producto",precio:2000},
+    {id:8 ,nombre:"Producto C" ,descripcion: "descripcion de producto",precio:2000},
+  ]
+  
+
+
+
+const getProductos = new Promise((resolve, reject) => {
+  if (productos.length > 0) {
+    setTimeout(() => {
+      resolve(productos);
+    }, 2000);
+  } else {
+    reject(new Error("No hay mas datos"));
+  }
+});
+
+getProductos
+.then((res) => {})
+.catch((error) => {
+  console.log(error);
+});
+
+
+
+return (
+  <>
+    <ItemList productos={productos} />  </>
+);
+};
+
 
 export default ItemListContainer
